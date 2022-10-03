c8720c647be14591c313e074f65ca7446ffeec26
Luke Barkess <luke.barkess@icloud.com>
2022-10-03 11:21:53 +0100

    asd

--- Performance/Program.cs
+++ Performance/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Performance
 {
@@ -10,6 +11,29 @@ namespace Performance
     {
         static void Main(string[] args)
         {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Test();
+            }
+
+            s.Stop();
+
+            Console.WriteLine($"Took {(float)s.ElapsedMilliseconds / 1000} seconds");
+
+            Console.ReadLine();
+        }
+
+        static void Test()
+        {
+            List<int> myList = new List<int>();
+
+            for (int i = 0; i < 49000000; i++)
+            {
+                myList.Add(i);
+            }
         }
     }
 }
